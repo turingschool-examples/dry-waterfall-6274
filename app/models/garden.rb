8ff738e2116eb3477d777bae184a5342bca6f6e3
Barry Allahyar <1475122+BarryA@users.noreply.github.com>
2024-03-04 12:32:07 -0600

    Add #harvest_under_hundred_days to garden model.

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,8 @@
 class Garden < ApplicationRecord
    has_many :plots
+   has_many :plants, through: :plots
+
+   def harvest_under_hundred_days
+      plants.distinct.where("days_to_harvest < ?", 100)
+   end
 end

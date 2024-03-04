1942436db19ef6d5b86ee906595e1b9209a1fd0b
Logan Finnegan <logantfinnegan@protonmail.com>
2024-03-04 12:56:59 -0600

    bells and whistles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/', to: 'welcome#index'
 
   get '/plots', to: 'plots#index'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+@garden = Garden.create!(name: "Larry's Garden", organic: true)
+
+@plot_1 = @garden.plots.create(number: 1, size: "large", direction: "west")
+@plot_2 = @garden.plots.create(number: 2, size: "small", direction: "east")
+
+@plant_1 = Plant.create!(name: "Rose", description: "red", days_to_harvest: 5)
+@plant_2 = Plant.create!(name: "Sunflower", description: "red", days_to_harvest: 115)
+
+PlotPlant.create!(plot_id: @plot_1.id, plant_id: @plant_1.id)
+PlotPlant.create!(plot_id: @plot_1.id, plant_id: @plant_2.id)
+PlotPlant.create!(plot_id: @plot_2.id, plant_id: @plant_2.id)

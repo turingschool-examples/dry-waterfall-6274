e160408c06bcb332ad9fcdfd51ebe60d4e4086da
Hoa Dam <hoangocdam@gmail.com>
2024-03-04 10:46:55 -0800

    feat:plants_harvest_under_100_days

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,6 +1,10 @@
 class Garden < ApplicationRecord
    has_many :plots
-
+   has_many :plants, through: :plots
    validates :name, presence: true
    validates :organic, presence: true, inclusion: { in: [true,false] }
+
+   def plants_harvest_under_100_days
+      plants.where("days_to_harvest < ?",100).distinct
+   end
 end

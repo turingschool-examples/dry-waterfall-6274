6039bc991c1a82c0c4316dd54806abfeb251a5bb
Martin Chavez <martinchavez1203@gmail.com>
2024-03-04 10:26:54 -0700

    test for US 2

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -49,7 +49,56 @@ RSpec.describe "Plots Index Page" do
         expect(page).to_not have_content('Tomato Plant')
       end
       # And under each plot number I see the names of all that plot's plants
-      save_and_open_page
+      # save_and_open_page
+    end
+  end
+
+  describe 'US 2 ' do
+    it 'Removes a plant from a plot' do
+      visit plots_path
+      # When I visit the plots index page
+
+      within ("#plot-#{@plot1.id}") do 
+        expect(page).to have_content('Tomato Plant')
+        expect(page).to have_button('Remove Tomato')
+        # Next to each plant's name
+
+        expect(page).to have_content('Lettuce Plant')
+        expect(page).to have_button('Remove Lettuce')
+        # I see a button to remove that plant from that plot
+
+        click_button("Remove Lettuce")
+      end
+
+      # When I click on that button
+
+      expect(current_path).to eq(plots_path)
+      # I'm returned to the plots index page
+
+      within ("#plot-#{@plot1.id}") do 
+        expect(page).to have_content('Tomato Plant')
+        expect(page).to have_button('Remove Tomato')
+
+        expect(page).to_not have_content('Lettuce Plant')
+        expect(page).to_not have_button('Remove Lettuce')
+        # And I no longer see that plant listed under that plot,
+
+      end
+     
+
+      within ("#plot-#{@plot2.id}") do 
+        expect(page).to have_content('Lettuce Plant')
+        expect(page).to have_button('Remove Lettuce')
+
+        # And I still see that plant's name under other plots that is was associated with.
+        expect(page).to have_content('Carrot Plant')
+        expect(page).to have_button('Remove Carrot')
+
+      end
+      
+      
+      # Note: you do not need to test for any sad paths or implement any flash messages. 
+      
     end
   end
 end

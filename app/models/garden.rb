fc293e308bdd1b387b7e66b0c63aa6bda841414e
Hoa Dam <hoangocdam@gmail.com>
2024-03-04 08:58:19 -0800

    feat:validates and add:relationship

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,6 @@
 class Garden < ApplicationRecord
    has_many :plots
+
+   validates :name, presence: true
+   validates :organic, presence: true, inclusion: { in: [true,false] }
 end
--- app/models/plot.rb
+++ app/models/plot.rb
@@ -1,3 +1,9 @@
 class Plot < ApplicationRecord
    belongs_to :garden
+   has_many :plant_plots
+   has_many :plants, through: :plant_plots
+
+   validates_presence_of :number,
+                         :size,
+                         :direction
 end

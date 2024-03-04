8e2e554a9a70753ad048a8ddef3dca9b4909e2ea
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-04 11:15:19 -0600

    us-1 plot index page

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -2,43 +2,43 @@ require "rails_helper"
 
 RSpec.describe "Plots Index", type: :feature do
   describe "As a visitor" do
-    let(:garden_1) { Garden.create!(name: "Leaf It", organic: true)}
-    let(:garden_2) { Garden.create!(name: "Garden the Plants from Harm", organic: false)}
+    let!(:garden_1) { Garden.create!(name: "Leaf It", organic: true)}
+    let!(:garden_2) { Garden.create!(name: "Garden the Plants from Harm", organic: false)}
 
-    let(:plot_1) { garden_1.plots.create!(number: 1, size: "small", direction: "north")}
-    let(:plot_2) { garden_1.plots.create!(number: 2, size: "medium", direction: "south")}
-    let(:plot_3) { garden_2.plots.create!(number: 3, size: "large", direction: "east")}
-    let(:plot_4) { garden_2.plots.create!(number: 4, size: "x-large", direction: "west")}
+    let!(:plot_1) { garden_1.plots.create!(number: 1, size: "small", direction: "north")}
+    let!(:plot_2) { garden_1.plots.create!(number: 2, size: "medium", direction: "south")}
+    let!(:plot_3) { garden_2.plots.create!(number: 3, size: "large", direction: "east")}
+    let!(:plot_4) { garden_2.plots.create!(number: 4, size: "x-large", direction: "west")}
 
-    let(:plant_1) {Plant.create! (name: "Pumpkin", description: "Halloween fun", days_to_harvest: 130)}
-    let(:plant_2) {Plant.create! (name: "Potato", description: "delicious", days_to_harvest: 35)}
-    let(:plant_3) {Plant.create! (name: "Cabbage", description: "rabbit food", days_to_harvest: 20)}
-    let(:plant_4) {Plant.create! (name: "Carrot", description: "better rabbit food", days_to_harvest: 101)}
-    let(:plant_5) {Plant.create! (name: "Tomato", description: "always taken off my sandwiches", days_to_harvest: 52)}
+    let!(:plant_1) {Plant.create!(name: "Pumpkin", description: "Halloween fun", days_to_harvest: 130)}
+    let!(:plant_2) {Plant.create!(name: "Potato", description: "delicious", days_to_harvest: 35)}
+    let!(:plant_3) {Plant.create!(name: "Cabbage", description: "rabbit food", days_to_harvest: 20)}
+    let!(:plant_4) {Plant.create!(name: "Carrot", description: "better rabbit food", days_to_harvest: 101)}
+    let!(:plant_5) {Plant.create!(name: "Tomato", description: "always taken off my sandwiches", days_to_harvest: 52)}
 
-    let(:plot_plant_1) {PlotPlant.create! (plot: plot_1, plant: plant_1)}
-    let(:plot_plant_2) {PlotPlant.create! (plot: plot_1, plant: plant_2)}
+    let!(:plot_plant_1) {PlotPlant.create!(plot: plot_1, plant: plant_1)}
+    let!(:plot_plant_2) {PlotPlant.create!(plot: plot_1, plant: plant_2)}
 
-    let(:plot_plant_3) {PlotPlant.create! (plot: plot_2, plant: plant_2)}
-    let(:plot_plant_4) {PlotPlant.create! (plot: plot_2, plant: plant_3)}
+    let!(:plot_plant_3) {PlotPlant.create!(plot: plot_2, plant: plant_2)}
+    let!(:plot_plant_4) {PlotPlant.create!(plot: plot_2, plant: plant_3)}
 
-    let(:plot_plant_5) {PlotPlant.create! (plot: plot_3, plant: plant_3)}
-    let(:plot_plant_6) {PlotPlant.create! (plot: plot_3, plant: plant_4)}
-    let(:plot_plant_7) {PlotPlant.create! (plot: plot_3, plant: plant_5)}
+    let!(:plot_plant_5) {PlotPlant.create!(plot: plot_3, plant: plant_3)}
+    let!(:plot_plant_6) {PlotPlant.create!(plot: plot_3, plant: plant_4)}
+    let!(:plot_plant_7) {PlotPlant.create!(plot: plot_3, plant: plant_5)}
 
-    let(:plot_plant_8) {PlotPlant.create! (plot: plot_4, plant: plant_1)}
-    let(:plot_plant_9) {PlotPlant.create! (plot: plot_4, plant: plant_4)}
-    let(:plot_plant_10) {PlotPlant.create! (plot: plot_4, plant: plant_5)}
+    let!(:plot_plant_8) {PlotPlant.create!(plot: plot_4, plant: plant_1)}
+    let!(:plot_plant_9) {PlotPlant.create!(plot: plot_4, plant: plant_4)}
+    let!(:plot_plant_10) {PlotPlant.create!(plot: plot_4, plant: plant_5)}
     
     before do
       visit "/plots"
     end
 
     it "displays all plot numbers" do
-      expect(page).to have_content("Plot 1")
-      expect(page).to have_content("Plot 2")
-      expect(page).to have_content("Plot 3")
-      expect(page).to have_content("Plot 4")
+      expect(page).to have_content("Plot #{plot_1.id}")
+      expect(page).to have_content("Plot #{plot_2.id}")
+      expect(page).to have_content("Plot #{plot_3.id}")
+      expect(page).to have_content("Plot #{plot_4.id}")
     end
 
     it "lists each plot's plants" do

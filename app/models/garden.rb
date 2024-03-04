8157e260d8bcc4ca7bbf29cdf568b810050be10b
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-04 10:55:57 -0700

    Refactor: transfer view logic to garden instance methods

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,4 +1,14 @@
 class Garden < ApplicationRecord
    has_many :plots
    has_many :plants, through: :plots
+
+   def unique_plant_list
+      plants.distinct
+   end
+
+   def upcoming_harvest_list
+      unique_plant_list.find_all do |plant|
+         plant.days_to_harvest.to_i <= 100
+      end
+   end
 end

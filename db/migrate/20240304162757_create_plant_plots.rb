f4030d7f8e3ad6d83ef3128a1ffeb5efbac36481
Barry Allahyar <1475122+BarryA@users.noreply.github.com>
2024-03-04 10:40:09 -0600

    Fix spelling error >.<

--- db/migrate/20240304162757_create_plant_plots.rb
+++ db/migrate/20240304162757_create_plant_plots.rb
@@ -1,6 +1,6 @@
-class CreatePlantPlots < ActiveRecord::Migration[7.1]
+class CreatePlotPlants< ActiveRecord::Migration[7.1]
   def change
-    create_table :plant_plots do |t|
+    create_table :plot_plants do |t|
       t.references :plant, null: false, foreign_key: true
       t.references :plot, null: false, foreign_key: true
 
